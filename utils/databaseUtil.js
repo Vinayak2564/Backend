cf301533b899fa16f57adcc415fa0c26dc41329a
Vinayak2564 <vinayakgundi9@gmail.com>
2026-08-21 23:53:02 +0530

    introduced NoSQL

--- utils/databaseUtil.js
+++ utils/databaseUtil.js
@@ -1,10 +1,17 @@
-const mysql = require('mysql2');
+const mongo = require ('mongodb');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root' ,
-  password: 'Vinayak_2564',
-  database: 'airbnb'
+const MongoClient = mongo.MongoClient;
+
+const MONGO_URL = "mongodb+srv://huli96167_db_user:<MWvaogc7ZqywPyYw>@cluster0.ygu9jpe.mongodb.net/?appName=Cluster0";   
+
+
+
+const mongoConnect = (callback) => {
+MongoClient.connect(MONGO_URL).then(client =>{
+  console.log(client);
+}).catch(err => {
+  console.log('Error While connecting to Mongo', err);
 });
+}
 
-module.exports = pool.promise();
+module.exports = mongoConnect; 
